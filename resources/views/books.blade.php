362ee87639b6a987dc7a4ab482b13d30b50e8fc2
Elshan <elshanqadimov7@gmail.com>
2022-03-21 01:22:58 +0100

    books show is working dummy version

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -1,33 +1,78 @@
 @extends('layouts.base')
 
 @section('content')
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td colspan="2">Larry the Bird</td>
-        <td>@twitter</td>
-      </tr>
-    </tbody>
-  </table>
+<div class="row">
+  <div class="col-sm-3 my-3">
+        <div class="card h-100">
+            <img src="https://edit.org/images/cat/book-covers-big-2019101610.jpg" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Book Name</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content</p>
+                {{-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
+                <a href="#" class="btn btn-primary">Open</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-3 my-3">
+        <div class="card h-100">
+            <img src="https://edit.org/images/cat/book-covers-big-2019101610.jpg" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Book Name</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content</p>
+                {{-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
+                <a href="#" class="btn btn-primary">Open</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-3 my-3">
+        <div class="card h-100">
+            <img src="https://edit.org/images/cat/book-covers-big-2019101610.jpg" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Book Name</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content</p>
+                {{-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
+                <a href="#" class="btn btn-primary">Open</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-3 my-3">
+        <div class="card h-100">
+            <img src="https://edit.org/images/cat/book-covers-big-2019101610.jpg" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Book Name</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content</p>
+                {{-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
+                <a href="#" class="btn btn-primary">Open</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-3 my-3">
+        <div class="card h-100">
+            <img src="https://edit.org/images/cat/book-covers-big-2019101610.jpg" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Book Name</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content</p>
+                {{-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
+                <a href="#" class="btn btn-primary">Open</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-3 my-3">
+        <div class="card h-100">
+            <img src="https://edit.org/images/cat/book-covers-big-2019101610.jpg" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Book Name</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content</p>
+                {{-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
+                <a href="#" class="btn btn-primary">Open</a>
+            </div>
+        </div>
+    </div>
+
+</div>
   @endsection
