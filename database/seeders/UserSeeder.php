65f5a703e880ae5d1a53962dceae1f1ffcd10443
Elshan <elshanqadimov7@gmail.com>
2022-04-11 01:06:23 +0200

    User module seeding complete

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->truncate();
+
+        User::create([
+            'name' => 'Jim Raynor',
+            'email' => 'jim.raynor@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        User::create([
+            'name' => 'Sarah Kerrigan',
+            'email' => 'sarah.kerrigan@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('12345678'),
+            'is_librarian' => true
+        ]);
+    }
+}
